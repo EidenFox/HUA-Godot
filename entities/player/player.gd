64e45539bcacf3f3cc2ee541719755bf2b12763c
EidenFox <3rcafe@gmail.com>
2026-09-04 22:16:52 -0300

    Adicionadas algumas mecanicas sobre a cor

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,40 +1,46 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 @onready var pivot: Node2D = $Pivot
 @onready var player_sprite: AnimatedSprite2D = $Pivot/PlayerSprite
 @onready var player_colision: CollisionShape2D = $PlayerColision
-
+@onready var color_rect: ColorRect = $ColorRect
 
 var djump = 0
 var baseDJump = 1
+
+func _ready() -> void:
+	color_rect.color = GlobalState.current_background_color
+	GlobalState.background_color_changed.connect(_on_background_color_changed)
+
+func _on_background_color_changed(new_color: Color) -> void:
+	color_rect.color = new_color
+
 func _physics_process(delta: float) -> void:
 	#double jump (adicionar função para couldown depois)
 	
 	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		
+	if is_on_floor():
+		djump = baseDJump
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor(): 
 		velocity.y = JUMP_VELOCITY
-		djump = baseDJump
+		#djump = baseDJump
 	
 	if Input.is_action_just_pressed("jump") and djump >= 1 and not is_on_floor():
 		djump -= 1
 		velocity.y = JUMP_VELOCITY-50
 	
-	
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
-	var run := Input.get
 	
 	if direction:
-		
 		if Input.is_action_pressed("run"):
 			velocity.x = direction * SPEED * 1.5
 			player_sprite.play("Run")
@@ -42,10 +48,9 @@ func _physics_process(delta: float) -> void:
 			velocity.x = direction * SPEED
 			player_sprite.play("Walk")
 		
-		# Quando o player muda de dureção
 		if direction < 0:
-			pivot.scale.x = -1 #inverte a sprite (com base no centro do desenho, não no centro da imagem)
-			player_colision.position.x = 50 #Centraliza a colision com base no centro do desenho
+			pivot.scale.x = -1
+			player_colision.position.x = 50
 		else:
 			pivot.scale.x = 1
 			player_colision.position.x = -50	
--- scripts/Global_state.gd
+++ scripts/Global_state.gd
@@ -1,16 +1,18 @@
 extends Node
 
-# Sinal emitido sempre que a cor de fundo mudar
 signal background_color_changed(new_color: Color)
 
-var current_background_color: Color = Color.BLACK:
+var current_background_color: Color = Color.html("#a7a7a7"):
 	set(value):
 		current_background_color = value
+		RenderingServer.set_default_clear_color(value)
 		background_color_changed.emit(value)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_O:
-			current_background_color = Color.html("#fc00ff")
-		elif event.keycode == KEY_P:
-			current_background_color = Color.BLACK
+		if event.keycode == KEY_UP:
+			current_background_color = Color.html("#FF0000")
+		elif event.keycode == KEY_LEFT:
+			current_background_color = Color.html("00FF00")
+		elif event.keycode == KEY_RIGHT:
+			current_background_color = Color.html("0000FF")
